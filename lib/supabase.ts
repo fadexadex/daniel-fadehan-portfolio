2156c8e7e4bb3638c41a3b9368cdbe03e7ded2e2
Danny944 <fadehandaniel2006@gmail.com>
2025-06-11 09:36:20 +0100

    fix admin issue

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js"
 // Create a single supabase client for interacting with your database
 export const createServerSupabaseClient = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-  const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
   return createClient(supabaseUrl, supabaseKey)
 }
 
@@ -25,6 +25,6 @@ export const getClientSupabaseClient = () => {
 }
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
